8b770b2c089e26b88bb18fe88ccc67d8cd1b195d
Javier <jafamo@gmail.com>
2025-04-22 13:49:03 +0200

    ADD feature/add-dependencies configure monolog

--- src/Infrastructure/Controller/PruebaController.php
+++ src/Infrastructure/Controller/PruebaController.php
@@ -4,9 +4,11 @@ namespace App\Infrastructure\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PruebaController extends AbstractController
 {
+
     public function index(): Response
     {
         return new Response("hola symfony");
