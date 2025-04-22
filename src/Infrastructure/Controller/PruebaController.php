ecd343ec0cbcc4a90364a509e3017faf17a509ad
Javier <jafamo@gmail.com>
2025-04-22 12:26:57 +0200

    Created pruebas controller

---
+++ src/Infrastructure/Controller/PruebaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Infrastructure\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class PruebaController extends AbstractController
+{
+    public function index(): Response
+    {
+        return new Response("hola symfony");
+    }
+}
